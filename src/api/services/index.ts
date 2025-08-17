a7a8e0beeb6f1c49585705eee47057e580c803ee
andi-frame <andifarhan1094@gmail.com>
2025-08-17 16:12:21 +0800

    feat: add index for preference service

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,3 +1,4 @@
 export { authService } from "./authService";
 export { tripsService } from "./tripsService";
 export { usersService } from "./usersService";
+export { preferencesService } from "./preference.service";
